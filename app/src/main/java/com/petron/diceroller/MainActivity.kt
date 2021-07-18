d491cd18c88164751e5ac3e0d36bbbd5bfc61fc5
Petron <androidtorrejoncillo1@gmail.com>
2021-07-18 15:11:47 +0200

    Añadido ejercicio y boton de borrar

--- app/src/main/java/com/petron/diceroller/MainActivity.kt
+++ app/src/main/java/com/petron/diceroller/MainActivity.kt
@@ -2,16 +2,30 @@ package com.petron.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    // Para qu no este continuanmente llamando la fiew aunque aqui no se utiliza
+   lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage= dice_Image
+
         roll_button.setOnClickListener { rollDice() }
         //result_text.text = "Dice Roller"
+        clear_button.setOnClickListener { clear() }
+
+
+    }
+    // Opcion de borrar la imagen
+    private fun clear(){
+        val borrar = R.drawable.empty_dice
+        diceImage.setImageResource(borrar)
 
     }
 
@@ -25,7 +39,31 @@ class MainActivity : AppCompatActivity() {
             5-> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        dice_image.setImageResource(drawableResource)
+        diceImage.setImageResource(drawableResource)
+
+/*
+    private fun rollDice() {
+        dice_Image.setImageResource(getRandomDiceImage())
+        dice_Image2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
+        val randomInt = (1..6).random()
+
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+
+ */
+
+
+
 /*
         val randomInt = (1..6).random()
         result_text.text = randomInt.toString()
@@ -41,4 +79,5 @@ class MainActivity : AppCompatActivity() {
  */
 
     }
+
 }
